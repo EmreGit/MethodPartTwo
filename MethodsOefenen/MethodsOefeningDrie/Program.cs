0e0475b4147c249705d3fd5506f9a19b02815d88
Emre Elagoz <Emre.Elagoz@student.intecbrussel.be>
2020-10-22 22:35:49 +0200

    Added a bunch of new ones

---
+++ MethodsOefenen/MethodsOefeningDrie/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodsOefeningDrie
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int userInput = int.Parse(Console.ReadLine());
+            Console.Write(" The digits in the number {0} are : ", userInput);
+
+        }
+
+        static void GetLastDigit(int digit)
+        {
+            
+            if (digit < 5)
+            {
+                
+                return;
+                GetLastDigit(digit / 10);
+                Console.Write(" {0} ", "vijf");
+                Console.WriteLine("{0} vijf", digit);
+            }
+
+            
+
+        }
+        
+    }
+}
---
+++ RefTypesValueTypes/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RefTypesValueTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Value Types
+            //int numberOne = 5;
+            //IncreaseByOne(numberOne);
+
+            //Console.WriteLine($"The value is: {numberOne}");
+            #endregion
+            int[] numbers = {1, 2, 3, 4, 5 };
+            IncreaseByOne(numbers);
+
+            Console.WriteLine(numbers[0]);
+        }
+
+        #region static void IncreaseByOne(int number)
+        //{
+        //    number++;
+        //}
+        #endregion
+
+        static void IncreaseByOne(int[] a)
+        {
+            a[0] = 100;
+            foreach (var item in a)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}

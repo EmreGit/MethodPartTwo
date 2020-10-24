e956630465be052dde4df9f0e498d7428fa1bc64
Emre Elagoz <Emre.Elagoz@student.intecbrussel.be>
2020-10-24 12:42:16 +0200

    changed

--- MethodsOefenen/MethodsOefeningDrie/Program.cs
+++ MethodsOefenen/MethodsOefeningDrie/Program.cs
@@ -11,26 +11,58 @@ namespace MethodsOefeningDrie
         static void Main(string[] args)
         {
 
-            int userInput = int.Parse(Console.ReadLine());
-            Console.Write(" The digits in the number {0} are : ", userInput);
+            //GreetTheUser();
+            NumberToGreet();
+
 
         }
 
-        static void GetLastDigit(int digit)
+        //static void GetLastDigit(int digit)
+        //{
+
+        //    if (digit < 5)
+        //    {
+        //        Console.WriteLine("{0} vijf", digit);
+        //        return;
+        //    }
+
+        //    GetLastDigit(digit / 10);
+        //    Console.Write(" {0} ", digit % 10);
+
+        //    num = Math.Abs(num);
+        //    string ans = Convert.ToString(num);
+        //    int i = ans.Length;
+        //    return ans[i = nth];
+
+        //}
+
+        //static void GreetTheUser()
+        //{
+        //    Console.WriteLine("Please type in a number: ");
+        //    int userInput = int.Parse(Console.ReadLine());
+
+        //    for (int i = userInput; i <= 20; i++)
+        //    {
+        //        Console.WriteLine("##############");
+        //        Console.WriteLine(i);
+        //    }
+        //}
+
+        static void NumberToGreet()
         {
-            
-            if (digit < 5)
+            Console.WriteLine("Please write your name: ");
+            string userName = Console.ReadLine();
+
+            if (true)
             {
-                
-                return;
-                GetLastDigit(digit / 10);
-                Console.Write(" {0} ", "vijf");
-                Console.WriteLine("{0} vijf", digit);
+                Console.WriteLine("Hello {0} ", userName);
             }
 
-            
-
         }
-        
+
     }
 }
+  
+
+
+      
